e330ccfdb63cdf60a269e1038b04dd77e83835c0
Ken Garber <kengarber98@gmail.com>
2019-03-22 12:22:58 -0400

    movie search page

--- src/routes.js
+++ src/routes.js
@@ -1,3 +1,5 @@
+var request = require('request');
+
 module.exports = function(app) {
   
   app.get('/', function(req, res) {
@@ -9,6 +11,23 @@ module.exports = function(app) {
 
   app.get('/other', function(req, res) {
     res.render('testpage')
+  })
+
+  app.get('/search', function(req, res) {
+  	let movieName = req.query.moviename
+  	if (movieName) {
+  		request(
+  			'http://www.omdbapi.com/?t=' + movieName + '&apiKey=' + process.env.OMDB_API_KEY, 
+  			(error, response, body) => {
+  				console.log(body)
+  				res.render('search', {
+  					moviename: movieName,
+  					searchresult: JSON.parse(body)
+  				})
+  			});
+  	} else {
+  		res.render('search', {moviename: movieName})
+  	}
   })
 
 }
