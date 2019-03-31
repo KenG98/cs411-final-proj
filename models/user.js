4c8060c3f1c01c3fdb9ab14842eb8f25bde39130
YongLAGCC <1291001957.yz@gmail.com>
2019-03-31 17:47:12 -0400

    mongoDB set up

--- models/user.js
+++ models/user.js
@@ -1,6 +1,6 @@
 var mongoose = require ('mongoose');
 var Schrma = mongoose.Schema; 
-
+// schema for users' login
 var userSchema = new Schema ({
     email: {type: String, required: true},
     password: {type: String, required: true }
--- src/db.js
+++ src/db.js
@@ -9,7 +9,6 @@ module.exports = {  // pass to routes
 	setup
 }
 
-
 var films = new Product({ 
     title: 'Stars War',
     year: '00000000000',
@@ -17,13 +16,13 @@ var films = new Product({
     director: 'Ken'
 });
 
-films.save().then({
+films.save().then({ // same model to our mongoDB
     if(err) {
         return err;
     }   
 });  
 
-function setup(app) {
+function setup(app) { // get movie info for clients
     app.get('/movie', function(req, res) {
         Product.find(function(err, movie) {
             res.render('movie', {title: 'Movie U LOVE', products: movie})
