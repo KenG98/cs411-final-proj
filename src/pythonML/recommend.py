e5eaa06acbda788aab89d401c729b240e23877b3
varunmalikayil <var.malikayil@gmail.com>
2019-04-26 17:14:31 -0400

    Added all Python files
    recommend.py is the file to run with the comma seperated list of imdb ids and returns the top 25 recommendations

---
+++ src/pythonML/recommend.py
@@ -0,0 +1,59 @@
+import pandas as pd
+import numpy as np
+import sys
+import operator
+
+movies_df = pd.read_csv("clustered_movie_ratings_1960-2019.csv", encoding = "ISO-8859-1")
+
+clusters = np.array(movies_df["Cluster"])
+
+imdbID = np.array(movies_df["IMDb ID"])
+
+watched = sys.argv[1]
+
+watched = np.array(watched.split(','))
+
+watched = np.array(list(dict.fromkeys(watched)))
+
+imdb_ratings = np.array(movies_df["Imdb"])
+
+votes = np.array(movies_df["Votes"])
+
+weighted = votes*imdb_ratings
+
+movies = list(zip(clusters,imdbID,weighted))
+
+movies.sort(key = operator.itemgetter(0))
+
+specificClusters = np.zeros(watched.size)
+for i in range(watched.size):
+	for j in range (clusters.size):
+		if(movies[j][1] == watched[i]):
+			specificClusters[i] = movies[j][0]
+specificClusters = np.array(list(dict.fromkeys(specificClusters)))
+
+recommend = []
+for i in range(clusters.size):
+	for j in range(specificClusters.size):
+		if(movies[i][0] == specificClusters[j]):
+			recommend.append(movies[i])
+
+
+recommend.sort(key = operator.itemgetter(2), reverse=True)
+
+finalRecommend = []
+for i in range(len(recommend)):
+	finalRecommend.append(recommend[i][1])
+
+for i in range(watched.size):
+	finalRecommend.remove(watched[i])
+
+movieList = ""
+if(len(finalRecommend) < 25):
+	for i in range(len(finalRecommend)):
+		movieList = movieList + finalRecommend[i] + ","
+else:
+	for i in range(25):
+		movieList = movieList + finalRecommend[i]
+movieList = movieList[:-1]
+print(movieList)
