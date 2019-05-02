594422e7780ac37bf049fd020471902584c09d0f
David <dshen1@bu.edu>
2019-05-01 21:24:27 -0400

    deleted temp files and cleaned up repo

--- src/pythonML/print_movies.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Recommendations:"
-OUT="$(python recommend.py ,)"
-arrOUT=(${OUT//,/ })
-for i in "${arrOUT[@]}"
-do
-    curl -s "http://www.omdbapi.com/?i=${i}&apikey=83dd260b" | python -c "import sys, json; print(json.load(sys.stdin)['Title'])"
-done
